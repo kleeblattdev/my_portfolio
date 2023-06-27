642b0f3d0253a30546449f01564139a85443e272
Karin Lee <119490837+kleeblattdev@users.noreply.github.com>
2023-06-27 15:27:01 +0200

    project text

--- src/assets/pages/Projects.jsx
+++ src/assets/pages/Projects.jsx
@@ -1,7 +1,35 @@
 const Projects = () =>{
     return(
     <main className="projects">
-        <h1>Hello Projects!</h1>
+        <section>
+        <article>
+                <h4>FoodGaroo</h4>
+                <p>Das FoodGaroo Projekt ist unser Full-Stack Abschlussprojekt vom SuperCode Bootcamp gewesen.
+                Es wurde von einem Team aus drei Personen realisiert. Für das Frontend haben wir Vite und SCSS genutzt.
+                NodeJS, ExpressJS und MongoDB würden für das Backend verwendet.
+                Dieses Projekt ist aktuell nur für mobile Geräte optimiert.</p>
+            </article>
+            <article>
+                <h4>Free2Game</h4>
+                <p>Free2Game war das Frontend-Abschlussprojekt von SuperCode. Dieses haben wir aus einem Team mit drei Personen mit ReactJS entwickelt.
+                    Die für das Projekt verwendeten Daten stammen aus der Free-To-Play Games API.
+                    Dieses Projekt funktioniert aufgrund der API leider nur auf dem Desktop.
+                </p>
+            </article>
+            <article>
+                <h4>Pokèdex</h4>
+                <p>Das Pokèdex Projekt ist ein reines Frontend Projekt
+                    Die Daten stammen aus der Pokèmon API.
+                </p>
+            </article>
+            <article>
+                <h4>Rock, Paper, Scissor</h4>
+                <p>Hast du Lust Schere, Stein, Papier zu spielen?
+                Dieses kleine Projekt habe ich mit HTML, CSS und JavaScript gebaut.
+                </p>
+                <a href="https://kleeblattdev.github.io/project_rock_paper_scissor/">Click hier zum Projekt</a>
+            </article>
+        </section>
     </main>)
     };
     export default Projects;
