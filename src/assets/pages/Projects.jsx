9e1ac2e31452a79cefde0886735c2432b75e712f
Karin Lee <119490837+kleeblattdev@users.noreply.github.com>
2023-06-28 11:15:02 +0200

    added links to projects

--- src/assets/pages/Projects.jsx
+++ src/assets/pages/Projects.jsx
@@ -8,6 +8,7 @@ const Projects = () =>{
                 Es wurde von einem Team aus drei Personen realisiert. Für das Frontend haben wir Vite und SCSS genutzt.
                 NodeJS, ExpressJS und MongoDB würden für das Backend verwendet.
                 Dieses Projekt ist aktuell nur für mobile Geräte optimiert.</p>
+                <a href="https://project-foodgaroo.onrender.com/">Click hier zum Projekt</a>
             </article>
             <article>
                 <h4>Free2Game</h4>
@@ -15,12 +16,14 @@ const Projects = () =>{
                     Die für das Projekt verwendeten Daten stammen aus der Free-To-Play Games API.
                     Dieses Projekt funktioniert aufgrund der API leider nur auf dem Desktop.
                 </p>
+                <a href="https://project-free2game.vercel.app/"> Click hier zum Projekt</a>
             </article>
             <article>
                 <h4>Pokèdex</h4>
                 <p>Das Pokèdex Projekt ist ein reines Frontend Projekt
                     Die Daten stammen aus der Pokèmon API.
                 </p>
+                <a href="https://pokedex-team-three.netlify.app/">Click hier zum Projekt</a>
             </article>
             <article>
                 <h4>Rock, Paper, Scissor</h4>
